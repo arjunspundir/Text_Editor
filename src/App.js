94bfaae29f45e55d9772b450d9655be60a7aeeb0
arjunspundir <arjunspundir154@gmail.com>
2023-10-17 17:23:50 +0530

    First basic look of my app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import TextForm from './components/TextForm';
+import Navigation from './components/Navigation';
 import './App.css';
+let name="Krishna" ;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <Navigation/>
+   <div className="container">
+   <TextForm heading={name}/>
+   </div>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useState } from 'react';
+let org="Inverse case";
+export default function TextForm(props) {
+    const wordLength=()=>{
+        let n=text;
+        n = n.trim();
+        let res=0;
+        // let word=true;
+        for(let i=0;i<n.length;i++){
+          if(n.charAt(i)===' ')
+          continue ;
+            while(i<n.length && n.charAt(i)!==' '){
+          i++;
+            }
+        res++ ;
+        }
+        return res;
+    }
+    const clear = ()=>{
+        changeText("");
+    }
+    const onchange=(event)=>{
+        changeText(event.target.value);
+        };
+        const clicked =()=>{
+            if(st==="lower"){
+            changeText(text.toUpperCase());
+            changest("upper");
+        }
+        else if(st==="upper"){
+            changeText(text.toLowerCase());
+            changest("lower");
+            }
+        
+        
+        };
+        const beautify =()=>{
+            let id=document.getElementById("my-box");
+            if(id.style.color==="black"){
+            id.style.backgroundColor="#4c1818";
+            id.style.color="white";
+            }
+            else{
+                id.style.backgroundColor="white";
+            id.style.color="black";
+            }
+        }
+    
+    const copy=()=>{
+       navigator.clipboard.writeText(text);
+    }
+    const [text , changeText]=useState("Enter here");
+    const [st , changest]=useState("lower");
+  return (
+    <>
+  <div className="mb-3">
+    <label htmlFor="my-box" className="form-label "><h3>Hare {props.heading} ,</h3><br/><h4>Enter your Text Below</h4></label>
+    <textarea className="form-control" id="my-box" rows="6" value={text} onChange={onchange}></textarea>
+    <button type="button" className="btn btn-secondary my-3 mx-2" onClick={clicked}>{org}</button>
+    <button type="button" className="btn btn-secondary my-3 mx-2" onClick={clear}>Clear</button>
+    <button type="button" className="btn btn-secondary my-3 mx-2"  id="beautify" onClick={beautify}>Bautify</button>
+    <button type="button" className="btn btn-secondary my-3 mx-2" onClick={copy}>Copy Text</button>
+  </div>
+    <div className="container">
+        <h5>Total :{wordLength()} words and {text.length} charcters</h5>
+    </div>
+    </>
+  )
+}
